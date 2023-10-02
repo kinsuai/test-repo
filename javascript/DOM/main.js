1edc7f392e098d41d8f72de65a344f807ea3ede4
kinsuai <opandejacob@gmail.com>
2023-10-02 11:47:25 +0200

    All folders hopefully added

---
+++ javascript/DOM/main.js
@@ -0,0 +1,21 @@
+// console.dir(document);
+// console.log(document.URL);
+// // console.log(document.domain);
+// console.log(document.title);
+// document.title="123456";
+//console.log(document.doctype);
+// console.log(document.head);
+//console.log(document.body);
+// console.log(document.all[10]);
+
+// console.log(document.all[8]);
+// // document.all[10]="Hello World";
+
+var RUBRIK=document.querySelector('h1');
+RUBRIK.innerText="MIN NYA RUBRIK";
+
+// // var LISTA=document.querySelector('li');
+// // LISTA.innerText="1-2-3-4-5";
+
+var LI=document.getElementsByTagName('li');
+LI.innerText="1-2-3-4-5";
---
+++ javascript/index.js
@@ -0,0 +1,35 @@
+//alert('hello World!!');
+//console.log('Hej på dig!!');
+//console.warn('det blev fel');
+let age1=30;
+let age2=40;
+console.log('suman av åldrarna är'+ (age1+age2));
+var age3=10;
+var age4=20;
+var summa =age3+age4;
+console.log(summa);
+//const namn="Jacob";
+//namn="David";
+//console.log(namn);
+let bilar=["volvo","saab","audi","volkswagen","Fiat"]
+bilar.push('mercedes')
+bilar.unshift('Ferari');
+
+
+for (let index = 0; index < bilar.length; index++) {
+	console.log(bilar[index]);
+	
+}
+
+for (var i = 0; i < 10; i++) 
+{
+	var sum=0
+	sum +=i
+	
+}
+console.log('summa är'+ sum);
+
+
+
+
+
